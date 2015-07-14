47b9ab3b65a4289942cb54f0461eec4ff98c391f
Dillon Shook <dshook@alumni.nmt.edu>
2015-07-13 22:43:16 -0400

    Pressure plate + random color shaders

---
+++ Puzzle Sandbox/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressurePlate : MonoBehaviour {
+	BoxCollider collide;
+	Renderer rend;
+
+	void Awake(){
+		collide = GetComponent<BoxCollider>();
+		rend = GetComponent<Renderer>();
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+		   
+	void OnTriggerEnter (Collider other)
+	{
+		var color = rend.material.color;
+		color.r += 40;
+		rend.material.color = color;
+	}
+		
+		
+	void OnTriggerExit (Collider other)
+	{
+		var color = rend.material.color;
+		color.r -= 40;
+		rend.material.color = color;
+	}
+}
