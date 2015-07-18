d98d7111c2d31cbf81ff033a66919fb6444f1e70
Dillon Shook <dshook@alumni.nmt.edu>
2015-07-18 18:51:41 -0400

    Camera zoom

---
+++ Puzzle Sandbox/Assets/Standard Assets/Cameras/Scripts/Zoom.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Zoom : MonoBehaviour {
+	public float zoomSpeed = 10f;
+	public float minDistance = 2.5f;
+	public float maxDistance = 6f;
+
+	Camera cam;
+	// Use this for initialization
+	void Start () {
+		cam = this.GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float scroll = CrossPlatformInputManager.GetAxis("Mouse ScrollWheel");
+		if(scroll != 0){
+			var newSize = cam.orthographicSize - (scroll * zoomSpeed);
+
+			newSize = Mathf.Clamp(newSize, minDistance, maxDistance);
+			cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, newSize, zoomSpeed * Time.deltaTime );
+
+		}
+
+	}
+}
